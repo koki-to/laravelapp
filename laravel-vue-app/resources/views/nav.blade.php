6cb063defe73dad9b294c21fca67a78163386b45
koki <niaoyuexingshu@gmail.com>
2020-09-22 15:30:16 +0900

    second commit

---
+++ laravel-vue-app/resources/views/nav.blade.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand navbar-dark blue-gradient">
+  <a href="/" class="navbar-brand"><i class="far fa-sticky-note mr-1"></i>memo</a>
+  <ul class="navbar-nav ml-auto">
+    <li class="nav-item">
+      <a href="" class="nav-lik">ユーザー登録</a>
+    </li>
+    <li class="nav-item">
+      <a href="" class="nav-link">ログイン</a>
+    </li>
+    <li class="nav-item">
+      <a href="" class="nav-link"><i class="fas fa-pen mr-1"></i>投稿する</a>
+    </li>
+    <!--Dropdown-->
+    <li class="nav-item dropdown">
+      <a href="" class="nav-link dropdown-toggle" id=""></a>
+    </li>
+
+  </ul>
+</nav>
