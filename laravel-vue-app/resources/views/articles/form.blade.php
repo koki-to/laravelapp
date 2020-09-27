1d6a1f5dacc60df19b02abd7d2b68cfff99efaad
koki <niaoyuexingshu@gmail.com>
2020-09-27 17:31:34 +0900

    Article add

---
+++ laravel-vue-app/resources/views/articles/form.blade.php
@@ -0,0 +1,9 @@
+@csrf
+<div class="md-form">
+  <label>タイトル</label>
+  <input type="text" class="form-control" required value="{{ old('title')}}">
+</div>
+<div class="form-group">
+  <label></label>
+  <textarea name="body" required rows="16" placeholder= "本文" class="form-control">{{ old('body') }}</textarea>
+</div>
