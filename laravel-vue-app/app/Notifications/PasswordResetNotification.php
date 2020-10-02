c6d4eb7b35e3e827ba60b6045046c0968681db83
koki <niaoyuexingshu@gmail.com>
2020-10-02 17:32:53 +0900

    ArticleLike.vue add

--- laravel-vue-app/app/Notifications/PasswordResetNotification.php
+++ laravel-vue-app/app/Notifications/PasswordResetNotification.php
@@ -2,6 +2,7 @@
 
 namespace App\Notifications;
 
+use App\Mail\BareMail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
@@ -10,15 +11,17 @@ use Illuminate\Notifications\Notification;
 class PasswordResetNotification extends Notification
 {
     use Queueable;
-
+    public $token;
+    public $mail;
     /**
      * Create a new notification instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(string $token, BareMail $mail)
     {
-        //
+        $this->token = $token;
+        $this->mail = $mail;
     }
 
     /**
@@ -40,10 +43,23 @@ class PasswordResetNotification extends Notification
      */
     public function toMail($notifiable)
     {
-        return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
+        return $this->mail
+          ->from(config('mail.from.address'), config('mail.from.name'))
+          ->to($notifiable->email)
+          ->subject('[memo]パスワード再設定')
+          ->text('email.password_reset')
+          ->with([
+              'url' => route('password.reset',[
+                  'token' => $this->token,
+                  'email' => $notifiable->email,
+
+              ]),
+              'count' => config(
+                  'auth.passwords.'.
+                  config('auth.defaults.passwords').
+                  '.expire'
+              ),
+          ]);
     }
 
     /**
